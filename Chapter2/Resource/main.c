191b4c1003437ef6b33690c9c2de0875f1c0137b
kiandr <kian.davoudirad@sage.com>
2015-12-08 16:22:09 -0800

    Adding  new example

--- Chapter2/Resource/main.c
+++ Chapter2/Resource/main.c
@@ -1,8 +1,17 @@
-#include "stdlib.h"
-#include "stdio.h"
-#include "27.h"
+#include <iostream>
+#include <ctime>
 
-int main(){
+int time()
+{
+	// current date/time based on current system
+	time_t now = time(0);
 
-	int t = Solution27();
-};
+
+	tm *lt = localtime(&now);
+	char dateObject[25];
+
+	sprintf_s(dateObject, sizeof(dateObject), "%d-%d-%dT%d:%d:%d.%dZ", (1900 + lt->tm_year), lt->tm_mon, lt->tm_mday, lt->tm_hour, lt->tm_min, lt->tm_sec);
+	printf(dateObject);
+
+	return true;
+}

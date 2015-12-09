a5c09a16ba5e0ea0784ea169ce1dc56d732c55ed
kiandr <kian.davoudirad@sage.com>
2015-12-08 16:23:35 -0800

    Time Parser

---
+++ Examples/TimeParser/TimeParser/main.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ctime>
+
+
+
+int main()
+{
+	// current date/time based on current system
+	time_t now = time(0);
+
+
+	tm *lt = localtime(&now);
+	char dateObject[25];
+
+	sprintf_s(dateObject, sizeof(dateObject), "%d-%d-%dT%d:%d:%d.%dZ", (1900 + lt->tm_year), lt->tm_mon, lt->tm_mday, lt->tm_hour, lt->tm_min, lt->tm_sec);
+	printf(dateObject);
+
+	return true;
+}

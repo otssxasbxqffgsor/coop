c81712ed3d653a717f7127989af0d2eabfc9203f
kian <davoudi.rk@gmail.com>
2016-01-12 11:46:49 -0800

    sdf

--- Examples/TestCrypto/CBC/CBC/CBC.cpp
+++ Examples/TestCrypto/CBC/CBC/CBC.cpp
@@ -1,90 +1,43 @@
 #include "stdafx.h"
 
-// g++ -g3 -ggdb -O0 -DDEBUG -I/usr/include/cryptopp Driver.cpp -o Driver.exe -lcryptopp -lpthread
-// g++ -g -O2 -DNDEBUG -I/usr/include/cryptopp Driver.cpp -o Driver.exe -lcryptopp -lpthread
+//:::::::: Standard C++ Libs ::://
 #include <windows.h>
-#include <cstdio>
-#include <iostream>
 
+#include <iostream>
 using std::cout;
 using std::cerr;
 using std::endl;
+#include <string>
+using std::string;
+#include <cstdlib>
+using std::exit;
+#include <cstdio>
+using std::cout;
+using std::cerr;
+using std::endl;
+
+//:::::::: Crypto ++ Libs ::://
 #include "..\cryptopp560\osrng.h"
 using CryptoPP::AutoSeededRandomPool;
-#include <cstdlib>
-using std::exit;
-#include "..\cryptopp560\modes.h"
-#include <iostream>
-using std::cout;
-using std::cerr;
-using std::endl;
-
-#include <string>
-using std::string;
-
-#include <cstdlib>
-using std::exit;
-
 #include "..\cryptopp560\cryptlib.h"
 using CryptoPP::Exception;
-
 #include "..\cryptopp560\hex.h"
 using CryptoPP::HexEncoder;
 using CryptoPP::HexDecoder;
-
 #include "..\cryptopp560\filters.h"
 using CryptoPP::StringSink;
 using CryptoPP::StringSource;
 using CryptoPP::StreamTransformationFilter;
-
 #include "..\cryptopp560\des.h"
 using CryptoPP::DES_EDE2;
-
 #include "..\cryptopp560\modes.h"
 using CryptoPP::CBC_Mode;
-
+using CryptoPP::ECB_Mode;
 #include "..\cryptopp560\secblock.h"
 using CryptoPP::SecByteBlock;
-#include <iostream>
-#include <string>
-#include "..\cryptopp560\modes.h"
-#include "..\cryptopp560\aes.h"
 #include"..\cryptopp560\filters.h"
-
-
-#include <iostream>
-using std::cout;
-using std::cerr;
-using std::endl;
-
-#include <string>
-using std::string;
-
-#include <cstdlib>
-using std::exit;
-
-#include "..\cryptopp560\cryptlib.h"
-using CryptoPP::Exception;
-
-#include "..\cryptopp560\hex.h"
-using CryptoPP::HexEncoder;
-using CryptoPP::HexDecoder;
-//using CryptoPP::Base64Encoder;
-#include "..\cryptopp560\filters.h"
-using CryptoPP::StringSink;
-using CryptoPP::StringSource;
-using CryptoPP::StreamTransformationFilter;
-
 #include "..\cryptopp560\aes.h"
 using CryptoPP::AES;
-
-#include "..\cryptopp560\modes.h"
-using CryptoPP::ECB_Mode;
-
-#include "..\\cryptopp560\base64.h"
-using CryptoPP::SimpleProxyFilter;
-using CryptoPP::Base64Encoder;
-
 #include "..\\cryptopp560\base64.h"
 using CryptoPP::SimpleProxyFilter;
 using CryptoPP::Base64Encoder;
@@ -124,78 +77,65 @@ string UriEncode(const string & sSrc)
 
 int main(int argc, char* argv[])
 {
-AutoSeededRandomPool prng;
+	AutoSeededRandomPool prng;
+	string plain = "businessPartnerId=22;sourceCompanyCode=22;sourceProduct=Sage100;fein=22;ts=2016-11-24T00:36:35.284Z;ec=0;";	
+	string cipher, encoded, recovered;
+	unsigned char key[] = {35,101,45,114,65,119,114,117,55,33,63,95,65,99,114,117,109,53,103,95,115,87,101,80,54,103,69,74,85,53,56,0};
+	string url;
+	string HttpUrl;
+	string urlEn;
 
-
-	// Filling with actual key
-	//string plain = "businessPartnerId=3;sourceCompanyCode=3;sourceProduct=Sage300;fein=3;ts=2015-12-21T19:59:06.812Z;ec=50;companyName=3;address1=3;address2=3;city=3;state=AK;zip=3;";
-	 string plain = "businessPartnerId=22;sourceCompanyCode=22;sourceProduct=Sage100;fein=22;ts=2016-12-24T00:36:35.284Z;ec=0;";	
-//string plain = "TestMeThisIsAGoodNews";
-string cipher, encoded, recovered;
-	unsigned char key[] = {35,101,45,114,65,119,114,117,55,33,63,95,65,99,114,117,109,
-53,103,95,115,87,101,80,54,103,69,74,85,53,56,0};
-
+	//Debugg
 	// Pretty print key
-	std::cout<<"The Key is->"<<key<< endl;;
+	cout<<"The Key is->"<<key<< endl;
+	cout << "plain text: " << plain << endl;
 	/*********************************\
 	\*********************************/
-
-try
-{
-    cout << "plain text: " << plain << endl;
-
     ECB_Mode< AES >::Encryption e;
     e.SetKey( key, sizeof(key) );
 
-    // The StreamTransformationFilter adds padding
-    //  as required. ECB and CBC Mode must be padded
-    //  to the block size of the cipher.
     StringSource ss1( plain, true, 
         new StreamTransformationFilter( e,
             new StringSink( cipher ),
 			StreamTransformationFilter::PKCS_PADDING
         ) // StreamTransformationFilter      
     ); // StringSource
-}
-catch( CryptoPP::Exception& e )
-	{
-    cerr << e.what() << endl;
-    exit(1);
-	}
 
-/*********************************\
-\*********************************/
+		/*********************************\
+		\**********Base64Encoder**********/
 
-// Pretty print cipher text
-StringSource ss2( cipher, true,
-    new Base64Encoder(
-        new StringSink( encoded )
-    ) // HexEncoder // Base64Encoder
-); // StringSource
-cout << "cipher text: " << encoded << endl;
+	StringSource ss2( cipher, true,
+		new Base64Encoder(
+			new StringSink( encoded )
+		) // HexEncoder // Base64Encoder
+	); // StringSource
+	
+	// Debugg
+	cout << "cipher text: " << encoded << endl;
+		/******Base64URLEncoder**********\
+		\********************************/
 
-/********************Base64URLEncoder*************\
-\*********************Base64Decoder************/
-string url;
-string urlEn =    encoded;
-string HttpUrl;
-StringSource ss(urlEn, true,
-    new Base64URLEncoder(
-        new StringSink(url)
-    ) // Base64URLDecoder
-); // StringSource
+		urlEn = encoded;
+
+	StringSource ss(urlEn, true,
+		 new Base64URLEncoder(
+		  new StringSink(url)
+		 ) // Base64URLDecoder
+		); // StringSource
  
 
-HttpUrl = "https://pgmorww11v.paigroup.corp/DDP.Web/Home/Sage100/?key="+url;
+
 
 /********************Base64URLEncoder*************\
 \*********************Base64Decoder************/
-urlEn = UriEncode(encoded);
-HttpUrl = "https://pgmorww11v.paigroup.corp/DDP.Web/Home/Sage100/?key="+urlEn;
+//HttpUrl = "https://pgmorww11v.paigroup.corp/DDP.Web/Home/Sage100/?key="+url;
+	//urlEn = UriEncode(encoded);
+	HttpUrl = "https://pgmorww11v.paigroup.corp/DDP.Web/Home/Sage100/?key="+UriEncode(encoded);
 
 /*********************************\
 \*********************************/
 // Decrypt
+/*
 try
 {
     ECB_Mode< AES >::Decryption d;
@@ -218,7 +158,7 @@ catch( CryptoPP::Exception& e )
     exit(1);
 }
 
-
+*/
 
 	return 0;
 }

d2f1bfccb4cc4e7b85e93832edabac5068fd48f5
kian <davoudi.rk@gmail.com>
2016-01-12 10:48:55 -0800

    setup DD

---
+++ BuildDD/setup.bat
@@ -0,0 +1,7 @@
+
+CALL C:\DD\pr\base\setproj UP
+CALL C:\DD\pr\base\SETUP32 UP
+CALL C:\DD\pr\base\cryptopp-lib-build-fetch jenkins-job
+CALL C:\DD\pr\base\cryptopp-lib-fetch jenkins-job
+CALL C:\DD\pr\base\build
+type build.err

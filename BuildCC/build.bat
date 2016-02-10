15ba707753027e05ae1ff4a3aba8e08ec9c45248
Kian <davoudi.rk@gmail.com>
2016-02-10 13:28:15 -0800

    Added the BAT file

--- BuildCC/build.bat
+++ BuildCC/build.bat
@@ -1,6 +1,11 @@
-ECHO "Step1: Copy mysetproj.bat to CC Direcotyr"
+@echo off
+set _arg=%1
+call :up.arg
+goto :EOF
 
-REM COPY the C:\CC\yp\base\mysetproj.bat to C:\CC\yp\base\mysetproj.bat
-XCOPY C:\GitHub\CC\mysetproj.bat C:\CC\yp\base\ /s /y
+:up.arg
+REM STEP 1: COPY the C:\CC\yp\base\mysetproj.bat to C:\CC\yp\base\mysetproj.bat
+XCOPY C:\GitHub\CrackingCodingInterview\BuildCC\mysetproj.bat C:\CC\yp\base\ /s /y
+goto :EOF
 
-echo "Step1: Completed"
+

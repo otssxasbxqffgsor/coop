9333f0d54c461fe6de2397b7e43fca50b2b7e31d
Kian <davoudi.rk@gmail.com>
2016-02-16 11:27:58 -0800

    Committing latest Change.

--- BuildCC/build.bat
+++ BuildCC/build.bat
@@ -6,6 +6,16 @@ goto :EOF
 :up.arg
 REM STEP 1: COPY the C:\CC\yp\base\mysetproj.bat to C:\CC\yp\base\mysetproj.bat
 XCOPY C:\GitHub\CrackingCodingInterview\BuildCC\mysetproj.bat C:\CC\yp\base\ /s /y
+pushd C:\CC\yp\base
+call  mysetproj.bat
+
+@echo on 
+echo "Calling Build"
+@echo off
+pushd C:\CC\yp\base\
+call  build.bat
+
+
 goto :EOF
 
 
